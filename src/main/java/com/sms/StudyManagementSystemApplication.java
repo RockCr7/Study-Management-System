b80041d37c07ccb9411fdf567bd23cbcca7654eb
CR7 <shoaibakhter3214@gmail.com>
2023-03-26 15:10:08 +0530

    Added required files to the project

---
+++ src/main/java/com/sms/StudyManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyManagementSystemApplication.class, args);
+	}
+
+}
